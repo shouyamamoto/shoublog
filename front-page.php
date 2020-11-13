d4bb14ef3e9debe29d14eef8b457e4144d92ac05
yamamoto syo <syo19961113@gmail.com>
2020-11-13 23:22:59 +0900

    記事一覧のスタイル追加

--- front-page.php
+++ front-page.php
@@ -1,4 +1,4 @@
-<?php get_header(); ?> <body><!-- メインビジュアル --><header class="mainVisual mb--sl"><img src="<?php echo get_template_directory_uri(); ?>/images/mainVisual.jpg" class="mainVisual__image" alt="やましょうのブログのメインビジュアルです。" width="100%" height="200px" style="object-fit: cover;"></header><!-- メインビジュアル --> <?php get_template_part('template-parts/nav', 'bar'); ?> <!-- よく読まれている記事 --><!-- 3件ループ --> <?php
+<?php get_header(); ?> <body><!-- メインビジュアル --><header class="mainVisual mb--md"><img src="<?php echo get_template_directory_uri(); ?>/images/mainVisual.jpg" class="mainVisual__image" alt="やましょうのブログのメインビジュアルです。" width="100%" height="200px"></header><!-- メインビジュアル --> <?php get_template_part('template-parts/nav', 'bar'); ?> <!-- よく読まれている記事 --><!-- 3件ループ --> <?php
         // views post metaで記事のPV情報を取得する
         setPostViews(get_the_ID());
 
@@ -9,20 +9,20 @@
             'posts_per_page' => 3, // ← 3件取得
         );
         $the_query = new WP_Query($args);
-    ?> <section class="mustPopular mb--md"><h2 class="mustPopular__title"><span class="mustPopular__title--num">01</span>MUST-POPULAR</h2><ul class="mustPopular__list"> <?php 
+    ?> <section class="popular mb--md"><h2 class="popular__title"><span class="popular__title--num">01</span>POPULAR</h2><ul class="popular__list"> <?php 
                 // WP_Queryによるループ
                 if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post();
-            ?> <li class="mustPopular__item"><div class="mustPopular__image"><a href="<?php the_permalink(); ?>" class="mustPopular__link"> <?php the_post_thumbnail('medium'); ?> </a></div><div class="mustPopular__details"><a href="<?php the_permalink(); ?>" class="mustPopular__link"> <?php the_title(); ?> </a></div></li> <?php endwhile; ?> <?php wp_reset_postdata(); ?> <?php else : ?> <p>記事がありません。</p> <?php endif; ?> </ul><span class="mustPopular__arrow--left"></span> <span class="mustPopular__arrow--right"></span></section><!-- よく読まれている記事 --><!-- 記事一覧 --><section class="article"><!-- codeの記事 --> <?php 
+            ?> <li class="popular__item"><div class="popular__image"><a href="<?php the_permalink(); ?>" class="popular__link"> <?php the_post_thumbnail('medium'); ?> </a></div><div class="popular__details"><a href="<?php the_permalink(); ?>" class="popular__link"> <?php the_title(); ?> </a></div></li> <?php endwhile; ?> <?php wp_reset_postdata(); ?> <?php else : ?> <p>記事がありません。</p> <?php endif; ?> </ul><span class="popular__arrow--left"></span> <span class="popular__arrow--right"></span></section><!-- よく読まれている記事 --><!-- 記事一覧 --><section class="article"><!-- codeの記事 --> <?php 
             $args = array(
                 'post_type' => 'post',
                 'category_name' => 'code',
                 'posts_per_page' => 4,
             );
             $the_query = new WP_Query($args);
-        ?> <article class="codeArticle"><h2 class="codeArticle__title"><span>02</span>CODE</h2><ul class="codeArticle__list"> <?php if ($the_query->have_posts()) :?> <?php while($the_query->have_posts()) : $the_query->the_post(); ?> <li class="codeArticle__item"><div class="codeArticle__image"><a href="<?php the_permalink(); ?>" class="codeArticle__link"> <?php the_post_thumbnail('medium'); ?> </a></div><div class="codeArticle__Details"><a href="<?php the_permalink(); ?>" class="codeArticle__link"><p><?php the_time('Y/n/j'); ?></p><p> <?php the_title(); ?> <?php the_excerpt(); ?> </p></a></div></li> <?php endwhile; ?> <?php wp_reset_postdata(); ?> <?php else: ?> <p>記事がありません。</p> <?php endif; ?> </ul></article><button class="codeArticle__btn"> <?php 
-                $category_id = get_cat_ID('code');
-                $category_link = get_category_link($category_id);
-            ?> <a href="<?php echo esc_url( $category_link ); ?>" class="codeArticle__link">もっと読む</a></button><!-- codeの記事 --><!-- photoの記事 --> <?php 
+        ?> <article class="codeArticle mb--lr"><h2 class="codeArticle__title"><span class="codeArticle__title--num">02</span>CODE</h2><ul class="codeArticle__list"> <?php if ($the_query->have_posts()) :?> <?php while($the_query->have_posts()) : $the_query->the_post(); ?> <li class="codeArticle__item mb--sm"><div class="codeArticle__image"><a href="<?php the_permalink(); ?>" class="codeArticle__link"> <?php if (has_post_thumbnail()) : ?> <?php the_post_thumbnail('medium'); ?> <?php else : ?> <img src="<?php echo get_template_directory_uri(); ?>/images/no-image.jpg" alt="サムネイルがありません。" width="300px" height="200px"> <?php endif; ?> </a></div><div class="codeArticle__details"><a href="<?php the_permalink(); ?>" class="codeArticle__link"><span class="codeArticle__time"><?php the_time('Y/n/j'); ?></span><br> <?php the_title(); ?> </a></div></li> <?php endwhile; ?> <?php wp_reset_postdata(); ?> <?php else: ?> <p>記事がありません。</p> <?php endif; ?> </ul><button class="codeArticle__btn"> <?php 
+                    $category_id = get_cat_ID('code');
+                    $category_link = get_category_link($category_id);
+                ?> <a href="<?php echo esc_url( $category_link ); ?>" class="codeArticle__link">もっと読む</a></button></article><!-- codeの記事 --><!-- photoの記事 --> <?php 
             $args = array(
                 'post_type' => 'post',
                 'posts_per_page' => 4,
@@ -30,17 +30,17 @@
             );
 
             $the_query = new WP_Query($args);
-        ?> <article class="photoArticle"><h2 class="photoArticle__title"><span>03</span>PHOTO</h2> <?php if ($the_query->have_posts()) :?> <ul class="photoArticle__list"> <?php while($the_query->have_posts()) : $the_query->the_post() ; ?> <li class="photoArticle__item"><div class="photoArticle__image"><a href="<?php the_permalink(); ?>" class="photoArticle__link"> <?php the_post_thumbnail('medium'); ?> </a></div><div class="photoArticle__Details"><a href="<?php the_permalink(); ?>" class="photoArticle__link"><p><?php the_time('Y/n/j'); ?></p><p> <?php the_title(); ?> <?php the_excerpt(); ?> </p></a></div></li> <?php endwhile; ?> <?php wp_reset_postdata(); ?> <?php else: ?> <p>記事がありません。</p> <?php endif; ?> </ul></article><button class="photoArticle__btn"> <?php 
+        ?> <article class="photoArticle mb--lr"><h2 class="photoArticle__title"><span class="photoArticle__title--num">03</span>PHOTO</h2> <?php if ($the_query->have_posts()) :?> <ul class="photoArticle__list"> <?php while($the_query->have_posts()) : $the_query->the_post() ; ?> <li class="photoArticle__item mb--sm"><div class="photoArticle__image"><a href="<?php the_permalink(); ?>" class="photoArticle__link"> <?php if (has_post_thumbnail()) : ?> <?php the_post_thumbnail('medium'); ?> <?php else : ?> <img src="<?php echo get_template_directory_uri(); ?>/images/no-image.jpg" class="no-image" alt="サムネイルがありません。" width="300px" height="200px"> <?php endif; ?> </a></div><div class="photoArticle__details"><a href="<?php the_permalink(); ?>" class="photoArticle__link"><span class="photoArticle__time"><?php the_time('Y/n/j'); ?></span><br> <?php the_title(); ?> </a></div></li> <?php endwhile; ?> <?php wp_reset_postdata(); ?> <?php else: ?> <p>記事がありません。</p> <?php endif; ?> </ul><button class="photoArticle__btn"> <?php 
                 $category_id = get_cat_ID('photo');
                 $category_link = get_category_link($category_id);
-            ?> <a href="<?php echo esc_url( $category_link ); ?>" class="photoArticle__link">もっと読む</a></button><!-- photoの記事 --><!-- lifeの記事 --> <?php 
+            ?> <a href="<?php echo esc_url( $category_link ); ?>" class="photoArticle__link">もっと読む</a></button></article><!-- photoの記事 --><!-- lifeの記事 --> <?php 
                 $args = array(
                     'post_type' => 'post',
                     'posts_per_page' => 3,
                     'category_name' => 'life',
                 );
                 $the_query = new WP_Query($args);
-        ?> <article class="lifeArticle"><h2 class="lifeArticle__title"><span>04</span>LIFE</h2> <?php if ($the_query->have_posts()):?> <ul class="lifeArticle__list"> <?php while($the_query->have_posts()) : $the_query->the_post(); ?> <li class="lifeArticle__item"><div class="lifeArticle__image"><a href="<?php the_permalink(); ?>" class="lifeArticle__link"> <?php the_post_thumbnail('medium'); ?> </a></div><div class="lifeArticle__Details"><a href="<?php the_permalink(); ?>" class="lifeArticle__link"><p><?php the_time('Y/n/j'); ?></p><p> <?php the_title(); ?> <?php the_excerpt(); ?> </p></a></div></li> <?php endwhile; ?> <?php wp_reset_postdata(); ?> <?php else: ?> <p>記事がありません。</p> <?php endif; ?> </ul></article><button class="lifeArticle__btn"> <?php 
+        ?> <article class="lifeArticle mb--lr"><h2 class="lifeArticle__title"><span class="lifeArticle__title--num">04</span>LIFE</h2> <?php if ($the_query->have_posts()):?> <ul class="lifeArticle__list"> <?php while($the_query->have_posts()) : $the_query->the_post(); ?> <li class="lifeArticle__item mb--sm"><div class="lifeArticle__image"><a href="<?php the_permalink(); ?>" class="lifeArticle__link"> <?php if (has_post_thumbnail()) : ?> <?php the_post_thumbnail('medium'); ?> <?php else : ?> <img src="<?php echo get_template_directory_uri(); ?>/images/no-image.jpg" alt="サムネイルがありません。" width="300px" height="200px"> <?php endif; ?> </a></div><div class="lifeArticle__details"><a href="<?php the_permalink(); ?>" class="lifeArticle__link"><span class="lifeArticle__time"><?php the_time('Y/n/j'); ?></span><br> <?php the_title(); ?> </a></div></li> <?php endwhile; ?> <?php wp_reset_postdata(); ?> <?php else: ?> <p>記事がありません。</p> <?php endif; ?> </ul><button class="lifeArticle__btn"> <?php 
                 $category_id = get_cat_ID('life');
                 $category_link = get_category_link($category_id);
-            ?> <a href="<?php echo esc_url( $category_link ); ?>" class="lifeArticle__link">もっと読む</a></button></section><!-- lifeの記事 --><!-- 自己紹介 --><section class="about"><h2 class="about__title"><span>05</span>ABOUT</h2><div class="about__item"><a href="" class="about__link"><img src="<?php echo get_template_directory_uri(); ?>/images/me.jpg" alt="#" width="100%" height="200px" style="object-fit: cover;"></a></div></section><!-- 自己紹介 --> <?php get_template_part('template-parts/category', 'list')?> <?php get_footer(); ?></body>
+            ?> <a href="<?php echo esc_url( $category_link ); ?>" class="lifeArticle__link">もっと読む</a></button></article></section><!-- lifeの記事 --><!-- 自己紹介 --><section class="about"><h2 class="about__title"><span>05</span>ABOUT</h2><div class="about__item"><a href="" class="about__link"><img src="<?php echo get_template_directory_uri(); ?>/images/me.jpg" alt="#" width="100%" height="200px"></a></div></section><!-- 自己紹介 --> <?php get_template_part('template-parts/category', 'list')?> <?php get_footer(); ?></body>
--- origin/src/html/front-page.php
+++ origin/src/html/front-page.php
@@ -1,8 +1,8 @@
 <?php get_header(); ?>
 <body>
     <!-- メインビジュアル -->
-    <header class="mainVisual mb--sl">
-        <img src="<?php echo get_template_directory_uri(); ?>/images/mainVisual.jpg" class="mainVisual__image" alt="やましょうのブログのメインビジュアルです。" width="100%" height="200px" style="object-fit: cover;">
+    <header class="mainVisual mb--md">
+        <img src="<?php echo get_template_directory_uri(); ?>/images/mainVisual.jpg" class="mainVisual__image" alt="やましょうのブログのメインビジュアルです。" width="100%" height="200px">
     </header>
     <!-- メインビジュアル -->
 
@@ -22,23 +22,23 @@
         );
         $the_query = new WP_Query($args);
     ?>
-    <section class="mustPopular mb--md">
-        <h2 class="mustPopular__title"><span class="mustPopular__title--num">01</span>MUST-POPULAR</h2>
+    <section class="popular mb--md">
+        <h2 class="popular__title"><span class="popular__title--num">01</span>POPULAR</h2>
 
-        <ul class="mustPopular__list">
+        <ul class="popular__list">
             <?php 
                 // WP_Queryによるループ
                 if ($the_query->have_posts()) : while ($the_query->have_posts()) : $the_query->the_post();
             ?>
-            <li class="mustPopular__item">
-                <div class="mustPopular__image">
-                    <a href="<?php the_permalink(); ?>" class="mustPopular__link">
+            <li class="popular__item">
+                <div class="popular__image">
+                    <a href="<?php the_permalink(); ?>" class="popular__link">
                         <?php the_post_thumbnail('medium'); ?>
                     </a>
                 </div>
 
-                <div class="mustPopular__details">
-                    <a href="<?php the_permalink(); ?>" class="mustPopular__link">
+                <div class="popular__details">
+                    <a href="<?php the_permalink(); ?>" class="popular__link">
                         <?php the_title(); ?>
                     </a>
                 </div>
@@ -49,8 +49,8 @@
                 <p>記事がありません。</p>
             <?php endif; ?>
         </ul>
-        <span class="mustPopular__arrow--left"></span>
-        <span class="mustPopular__arrow--right"></span>
+        <span class="popular__arrow--left"></span>
+        <span class="popular__arrow--right"></span>
     </section>
     <!-- よく読まれている記事 -->
 
@@ -65,23 +65,26 @@
             );
             $the_query = new WP_Query($args);
         ?>
-        <article class="codeArticle">
-            <h2 class="codeArticle__title"><span>02</span>CODE</h2>
+        <article class="codeArticle mb--lr">
+            <h2 class="codeArticle__title"><span class="codeArticle__title--num">02</span>CODE</h2>
             <ul class="codeArticle__list">
             <?php if ($the_query->have_posts()) :?>
             <?php while($the_query->have_posts()) : $the_query->the_post(); ?>
-                <li class="codeArticle__item">
+                <li class="codeArticle__item mb--sm">
                     <div class="codeArticle__image">
                         <a href="<?php the_permalink(); ?>" class="codeArticle__link">
-                        <?php the_post_thumbnail('medium'); ?>
+                            <?php if (has_post_thumbnail()) : ?>
+                                <?php the_post_thumbnail('medium'); ?>
+                            <?php else : ?>
+                                <img src="<?php echo get_template_directory_uri(); ?>/images/no-image.jpg" alt="サムネイルがありません。" width="300px" height="200px">
+                            <?php endif; ?>
                         </a>
                     </div>
                     
-                    <div class="codeArticle__Details">
+                    <div class="codeArticle__details">
                         <a href="<?php the_permalink(); ?>" class="codeArticle__link">
-                        <p><?php the_time('Y/n/j'); ?><p>
-                        <?php the_title(); ?>
-                        <?php the_excerpt(); ?>
+                            <span class="codeArticle__time"><?php the_time('Y/n/j'); ?></span><br>
+                            <?php the_title(); ?>
                         </a>
                     </div>
                 </li>
@@ -91,16 +94,17 @@
                 <p>記事がありません。</p>
             <?php endif; ?>
             </ul>
-        </article>
-        <button class="codeArticle__btn">
-            <?php 
-                $category_id = get_cat_ID('code');
-                $category_link = get_category_link($category_id);
-            ?>
-            <a href="<?php echo esc_url( $category_link ); ?>" class="codeArticle__link">もっと読む</a>
-        </button>
-        <!-- codeの記事 -->
 
+            <button class="codeArticle__btn">
+                <?php 
+                    $category_id = get_cat_ID('code');
+                    $category_link = get_category_link($category_id);
+                ?>
+                <a href="<?php echo esc_url( $category_link ); ?>" class="codeArticle__link">もっと読む</a>
+            </button>
+        </article>
+        
+        <!-- codeの記事 -->
 
         <!-- photoの記事 -->
         <?php 
@@ -112,23 +116,26 @@
 
             $the_query = new WP_Query($args);
         ?>
-        <article class="photoArticle">
-            <h2 class="photoArticle__title"><span>03</span>PHOTO</h2>
+        <article class="photoArticle mb--lr">
+            <h2 class="photoArticle__title"><span class="photoArticle__title--num">03</span>PHOTO</h2>
             <?php if ($the_query->have_posts()) :?>
             <ul class="photoArticle__list">
                 <?php while($the_query->have_posts()) : $the_query->the_post() ; ?>
-                <li class="photoArticle__item">
+                <li class="photoArticle__item mb--sm">
                     <div class="photoArticle__image">
                         <a href="<?php the_permalink(); ?>" class="photoArticle__link">
-                        <?php the_post_thumbnail('medium'); ?>
+                            <?php if (has_post_thumbnail()) : ?>
+                                <?php the_post_thumbnail('medium'); ?>
+                            <?php else : ?>
+                                <img src="<?php echo get_template_directory_uri(); ?>/images/no-image.jpg" class="no-image" alt="サムネイルがありません。" width="300px" height="200px">
+                            <?php endif; ?>
                         </a>
                     </div>
                     
-                    <div class="photoArticle__Details">
+                    <div class="photoArticle__details">
                         <a href="<?php the_permalink(); ?>" class="photoArticle__link">
-                        <p><?php the_time('Y/n/j'); ?><p>
-                        <?php the_title(); ?>
-                        <?php the_excerpt(); ?>
+                            <span class="photoArticle__time"><?php the_time('Y/n/j'); ?></span><br>
+                            <?php the_title(); ?>
                         </a>
                     </div>
                 </li>
@@ -138,14 +145,15 @@
                     <p>記事がありません。</p>
                 <?php endif; ?>
             </ul>
-        </article>
-        <button class="photoArticle__btn">
+            <button class="photoArticle__btn">
             <?php 
                 $category_id = get_cat_ID('photo');
                 $category_link = get_category_link($category_id);
             ?>
             <a href="<?php echo esc_url( $category_link ); ?>" class="photoArticle__link">もっと読む</a>
         </button>
+        </article>
+        
         <!-- photoの記事 -->
 
         <!-- lifeの記事 -->
@@ -157,23 +165,26 @@
                 );
                 $the_query = new WP_Query($args);
         ?>
-        <article class="lifeArticle">
-            <h2 class="lifeArticle__title"><span>04</span>LIFE</h2>
+        <article class="lifeArticle mb--lr">
+            <h2 class="lifeArticle__title"><span class="lifeArticle__title--num">04</span>LIFE</h2>
             <?php if ($the_query->have_posts()):?>
             <ul class="lifeArticle__list">
                 <?php while($the_query->have_posts()) : $the_query->the_post(); ?>
-                    <li class="lifeArticle__item">
+                    <li class="lifeArticle__item mb--sm">
                         <div class="lifeArticle__image">
                             <a href="<?php the_permalink(); ?>" class="lifeArticle__link">
-                            <?php the_post_thumbnail('medium'); ?>
+                                <?php if (has_post_thumbnail()) : ?>
+                                    <?php the_post_thumbnail('medium'); ?>
+                                <?php else : ?>
+                                    <img src="<?php echo get_template_directory_uri(); ?>/images/no-image.jpg" alt="サムネイルがありません。" width="300px" height="200px">
+                                <?php endif; ?>
                             </a>
                         </div>
                         
-                        <div class="lifeArticle__Details">
+                        <div class="lifeArticle__details">
                             <a href="<?php the_permalink(); ?>" class="lifeArticle__link">
-                            <p><?php the_time('Y/n/j'); ?><p>
-                            <?php the_title(); ?>
-                            <?php the_excerpt(); ?>
+                                <span class="lifeArticle__time"><?php the_time('Y/n/j'); ?></span><br>
+                                <?php the_title(); ?>
                             </a>
                         </div>
                     </li>
@@ -183,14 +194,15 @@
                     <p>記事がありません。</p>
                 <?php endif; ?>
             </ul>
-        </article>
-        <button class="lifeArticle__btn">
+            <button class="lifeArticle__btn">
             <?php 
                 $category_id = get_cat_ID('life');
                 $category_link = get_category_link($category_id);
             ?>
             <a href="<?php echo esc_url( $category_link ); ?>" class="lifeArticle__link">もっと読む</a>
         </button>
+        </article>
+        
     </section>
     <!-- lifeの記事 -->
 
@@ -199,7 +211,7 @@
         <h2 class="about__title"><span>05</span>ABOUT</h2>
         <div class="about__item">
             <a href="" class="about__link">
-                <img src="<?php echo get_template_directory_uri(); ?>/images/me.jpg" alt="#" width="100%" height="200px" style="object-fit: cover;">
+                <img src="<?php echo get_template_directory_uri(); ?>/images/me.jpg" alt="#" width="100%" height="200px">
             </a>
         </div>
     </section>
