4d0b71864057d799c1a1b9b7ff2e7a998f9fd1cc
yamamoto syo <syo19961113@gmail.com>
2020-11-16 14:47:49 +0900

    カテゴリー一覧のスタイル追加

--- origin/src/html/template-parts/category-list.php
+++ origin/src/html/template-parts/category-list.php
@@ -1,9 +1,9 @@
 <!-- カテゴリー一覧 -->
-<section class="category" id="category">
+<section class="category mb--lr" id="category">
     <h2 class="category__title">CATEGORY</h2>
     
     <!-- codeカテゴリーの子カテゴリー一覧 -->
-    <div class="codeCategory">
+    <div class="codeCategory mb--md">
         <img src="<?php echo get_template_directory_uri(); ?>/images/code.jpg" alt="" class="codeCategory__image" width="100%" height="200px" style="object-fit: cover;">
         <ul class="codeCategory__list">
             <?php 
@@ -28,8 +28,9 @@
     <!-- codeカテゴリーの子カテゴリー一覧 -->
 
     <!-- photoカテゴリーの子カテゴリー一覧 -->
-    <div class="photoCategory">
+    <div class="photoCategory mb--md">
         <img src="<?php echo get_template_directory_uri(); ?>/images/photo.jpg" alt="" class="photoCategory__image" width="100%" height="200px" style="object-fit: cover;">
+        <ul class="photoCategory__list">
         <?php 
             $parent_category = get_category_by_slug('photo');
             $args = array(
@@ -41,20 +42,20 @@
             $child_categories = get_categories($args);
             foreach($child_categories as $category) :
         ?>
-        <ul class="photoCategory__list">
             <li class="photoCategory__item">
                 <a href="<?php echo get_category_link($category->term_id); ?>" class="photoCategory__link">
                     <?php echo $category->cat_name; ?>
                 </a>
             </li>
-        </ul>
         <?php endforeach;?>
+        </ul>
     </div>
     <!-- photoカテゴリーの子カテゴリー一覧 -->
     
     <!-- lifeカテゴリーの子カテゴリー一覧 -->
-    <div class="lifeCategory">
+    <div class="lifeCategory mb--md">
         <img src="<?php echo get_template_directory_uri()?>/images/life.jpg" alt="" class="lifeCategory__image" width="100%" height="200px" style="object-fit: cover;">
+        <ul class="lifeCategory__list">
         <?php 
             $parent_category = get_category_by_slug('life');
             $args = array(
@@ -67,7 +68,6 @@
             $child_categories = get_categories($args);
             foreach($child_categories as $category):
         ?>
-        <ul class="lifeCategory__list">
             <li class="lifeCategory__item">
                 <a href="<?php echo get_category_link($category->term_id); ?>" class="lifeCategory__link">
                     <?php echo $category->cat_name; ?>
