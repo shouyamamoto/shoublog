884154db74b3cc629842de5ebfe948c7849234f3
yamamoto syo <syo19961113@gmail.com>
2020-11-18 22:44:26 +0900

    パンくずリスト追加

---
+++ template-parts/breadcrumb.php
@@ -0,0 +1,5 @@
+<div class="breadcrumb"></div><div class="breadcrumb__inner"> <?php
+            if(function_exists('bcn_display')) {
+                bcn_display();
+            }
+        ?> </div>
