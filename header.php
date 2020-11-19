ebdb673c9baa09ee3d436f75d161b33213be982e
yamamoto syo <syo19961113@gmail.com>
2020-11-20 08:07:39 +0900

    metaタグ追記

--- header.php
+++ header.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html lang="ja">
-<head>
+<head prefix=”og: http://ogp.me/ns#” >
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
+
+    <meta content="コーディング、プログラミングについて、また趣味である写真や生活のコトをお届けするブログです。コーディングやプログラミングの記事はリファレンスを参考にしながら解説しているので、初学者の方にもわかりやすい内容になっていると思います。" name="description">
+    <meta property="og:type" content="website" />
+    <meta property="og:url" content="shou.blog/" />
+    <meta property="og:image" content="shou.blog/images/mainVisual.jpg" />
+    <meta property="og:site_name"  content="やましょうのブログ" />
+    <meta property="og:description" content="コーディング、プログラミングについて、また趣味である写真や生活のコトをお届けするブログです。コーディングやプログラミングの記事はリファレンスを参考にしながら解説しているので、初学者の方にもわかりやすい内容になっていると思います。" />
+    <meta name="twitter:card" content="summary_large_image"/>
+    <meta name="twitter:site" content="@_syoyamamoto_" />
+
     <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/reset.css">
     <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/css/base.css">
