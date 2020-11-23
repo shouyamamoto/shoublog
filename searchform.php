6da37f5a15d8f4271f60576bb16f1470b59dbe3e
yamamoto syo <syo19961113@gmail.com>
2020-11-24 07:55:42 +0900

    searchformにカテゴリーを表示

--- searchform.php
+++ searchform.php
@@ -4,4 +4,13 @@
         <input type="text" name="s" value="<?php the_search_query(); ?>" class="search__form">
         <img src="<?php echo get_template_directory_uri(); ?>/images/search-solid.svg" alt="" class="search__icon">
     </div>
+
+    <ul class="search__list">
+    <?php
+        $categories = get_categories();
+        foreach($categories as $category) {
+        echo '<li class="search__item"><a href="'.get_category_link($category->term_id).'">'.$category->name.'</a></li>';
+        }
+        ?>
+    </ul>
 </form>
