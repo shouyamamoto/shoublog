277de5f4e020379dc8358132626bd4893096cd78
yamamoto syo <syo19961113@gmail.com>
2020-11-24 21:43:59 +0900

    twitterとinstagramへのリンク追加

--- front-page.php
+++ front-page.php
@@ -172,6 +172,10 @@
                 <img src="<?php echo get_template_directory_uri(); ?>/images/me.jpg" class="about__image" alt="#" width="100%" height="auto">
             </a>
         </div>
+        <div class="about__item">
+            <a href="https://twitter.com/_syoyamamoto_" class="about__item--sns" target="_blank" rel="noopener noreferrer">Twitter</a>
+            <a href="https://www.instagram.com/ymsy_1113" class="about__item--sns" target="_blank" rel="noopener noreferrer">Instagram</a>
+        </div>
     </section>
     <!-- 自己紹介 -->
     
